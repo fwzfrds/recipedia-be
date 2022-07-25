ecbcdfee5c1c95096a765135ac259545cbe42831
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-07-25 10:51:15 +0700

    Mengganti allow origin di cors soalnya hanya llow 1 saja

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Origin', ['https://pijarnext-fwzfrds.vercel.app/'])
+  res.header('Access-Control-Allow-Origin', ['https://pijarnext-fwzfrds.vercel.app'])
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
   next()
