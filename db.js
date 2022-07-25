648f2b545e5602b10a976af7cc30e5922e9f9c03
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-07-25 11:07:41 +0700

    Mencoba lagi cookie cross domain

--- db.js
+++ db.js
@@ -4,14 +4,14 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
   // ssl untuk di local
   // ssl: false
 
   // ssl untuk mengatasi error di heroku
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 module.exports = pool
