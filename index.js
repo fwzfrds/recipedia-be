d4e780d7d94b9d314ceb87908047bf4c99b9ab1c
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-07-25 12:21:05 +0700

    Menambahlan acces-control-expose-header pada index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', ['https://pijarnext-fwzfrds.vercel.app'])
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, set-cookie, Cookie')
-  res.header('Access-Control-Expose-Headers', ['set-cookie'])
+  res.header('Access-Control-Expose-Headers', ['Set-Cookie'])
   next()
 })
 // app.use(cors())
